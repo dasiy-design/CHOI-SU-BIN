fbcaa6de23b5e331293d239ae4216197510a2ea0
dasiy-design <z_o_ojj@naver.com>
2025-07-16 18:52:44 +0900

    0716 다중모달중
    드뎌!!!!

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,6 +1,28 @@
-const modal = document.querySelector(".modal");
-const btnOpenModal = document.querySelector(".btn-open-modal");
+var btn = document.querySelectorAll("button.modal-custom-button");
+var modals = document.querySelectorAll(".modal-custom");
+var spans = document.getElementsByClassName("close-modal");
 
-btnOpenModal.addEventListener("click", () => {
-  modal.style.display = "flex";
-});
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+  };
+
+  spans[i].onclick = function () {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+    }
+  };
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target.classList.contains("modal-custom")) {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+    }
+  }
+};
